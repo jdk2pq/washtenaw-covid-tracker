24e9407058255ca913d4d3721c1acfede392fe31
Jake Kenneally <jakekenneally@gmail.com>
2020-04-08 22:47:56 -0400

    added data from 4/8

--- scrape.js
+++ scrape.js
@@ -16,7 +16,8 @@ const fs = require('fs');
         totalCasesTable,
         byAgeGroupTable,
         bySexTable,
-        byZipCodeTable,
+        // Zip code table is gone as of 4/7
+        // byZipCodeTable,
         byRaceTable] = await page.evaluate((today, todayStr) => {
         const updated = document.querySelector('.fr-view p').textContent.trim().includes(`${todayStr}`)
 
@@ -39,10 +40,10 @@ const fs = require('fs');
         stringify(bySexTable, (err, output) => {
             return fs.appendFileSync("bySex.csv", output);
         });
-
-        stringify(byZipCodeTable, (err, output) => {
-            return fs.appendFileSync("byZipCode.csv", output);
-        });
+        // Zip code table is gone as of 4/7
+        // stringify(byZipCodeTable, (err, output) => {
+        //     return fs.appendFileSync("byZipCode.csv", output);
+        // });
 
         stringify(byRaceTable, (err, output) => {
             return fs.appendFileSync("byRace.csv", output);
